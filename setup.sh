ea9f4215a4a1d46cb449c9d9ae9ab97cafc36546
Steven Aston <Steven.James.Aston@gmail.com>
2017-07-28 03:40:12 -0400

    Upgrade command just causing too many problems

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get install -y build-essential curl nano git-core
 cd ~
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
